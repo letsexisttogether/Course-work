3c5b8ef27bb64c7c8a072714785e145e8bcdefa0
Alexsander Chernenko <71719320+letsexisttogether@users.noreply.github.com>
2023-01-17 18:26:47 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Login;
+using Registration;
+using View.MainPart;
+
+namespace Course_work
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// Главная точка входа для приложения.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new LoginForm());
+        }
+    }
+}
